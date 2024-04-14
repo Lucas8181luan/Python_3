d52cfed93d0a05a3e59e06c6015c810278d80b20
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 19:13:53 -0300

    Quantidade de Tinta Para Pintar Uma Parede

---
+++ 11 - Quantidade de Tinta Para Pintar Uma Parede.py
@@ -0,0 +1,25 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# COMO DESCOBRIR A QUANTIDADE DE TINTA NESCESSÁRIO PARA PINTAR UMA PAREDE
+
+# 1° Passo, ler a largura e a altura da parede
+largura = float(input("Qual a largura da parede = "))
+
+altura = float(input("Qual a altura da parede = "))
+
+# Faça a soma
+area = largura * altura
+
+print("A dimensão da parede e de {}x{} e sua área é der {}m²".format(largura, altura, area))
+
+# Descobrindo a quantidade de tinta
+tinta = area / 2
+
+print("A quantidade que você precisara e de = {}L de tinta".format(tinta))
+
+
+
+
