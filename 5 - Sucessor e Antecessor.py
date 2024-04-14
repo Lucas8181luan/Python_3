63a208c085405b18f1b83c40e6ace58a195fc996
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 15:56:20 -0300

    Antecessor e Sucessor

---
+++ 5 - Sucessor e Antecessor.py
@@ -0,0 +1,17 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# UMA PROGRAMAR QUE LEIA UM NÚMERO, E MOSTRE SEU ANTECESSOR E SUCESSOR
+
+# 1° Passo, Entrada do Número
+numero = int(input("Digite um Número = "))
+
+# Seu Antecessor
+antecessor = numero - 1
+
+# Seu Sucessor
+sucessor = numero + 1
+
+print("Analisando o Valor {} seu Antecessor e {} e seu Sucessor e {}".format(numero, antecessor, sucessor))
