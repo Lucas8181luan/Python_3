2780d64c62c546f086b93b6e995da6f6494e65f9
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 20:35:25 -0300

    Converter de °C para °F

---
+++ 14 - Converter Temperatura.py
@@ -0,0 +1,17 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# UM PROGRAMA QUE CONVERTA UMA TEMPERATURA DIGITADA EM °C PARA °F
+
+# 1 Passo, Entrada para digitar a temperatura
+temperatura = float(input("Informe a temperatura = "))
+
+# Faça a soma
+soma_1 = (9 * temperatura)
+soma_2 = soma_1 / 5
+soma_final = soma_2 + 32
+
+print("A temperatura de {}°C e igual a = {}°F".format(temperatura, soma_final))
+
