1f25935b9a8fb9e67db70ba3f49b634719a715e3
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 17:17:18 -0300

    Soma a Média da Nota de Um Aluno

---
+++ 7 - Calucular a Media de Aluno.py
@@ -0,0 +1,19 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# MOSTRA DUAS NOTAS DE UM ALUNO E CALCULAR A SUA MÉDIA
+
+# 1° Passo, Ler As Duas Notas
+nota_1 = float(input("Primeira Nota = "))
+
+nota_2 = float(input("Ler a Segunda Nota = "))
+
+# 2° Passo, realizar a soma
+soma = (nota_1 + nota_2) / 2
+
+print("A média entre {:.1f} e {:.1f} é igual a = {:.1f}".format(nota_1, nota_2, soma))
+
+
+
