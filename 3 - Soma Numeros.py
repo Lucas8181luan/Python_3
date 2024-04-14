538ff861de96765ed4182f0818d5d95fdbfe50c9
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 15:04:15 -0300

    Soma Dois Números

---
+++ 3 - Soma Numeros.py
@@ -0,0 +1,19 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# PARA SOMA DOIS NÚMEROS
+
+# 1° Passo, Ler Dois Numeros
+n1 = int(input("Digite o Primeiro Número = "))
+n2 = int(input("Digite o Segundo Número = "))
+
+# 2° Passo, Faça a Soma
+soma = n1 + n2
+
+#Resultado
+print("A soma entre {} é {} e igual a = {}".format(n1, n2, soma))
+
+
+
