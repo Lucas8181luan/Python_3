73f5cf14b779675dcaa2d327badc488b6aa751a4
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 18:42:26 -0300

    Tabuada

---
+++ 9 - Tabuada.py
@@ -0,0 +1,34 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# COMO MONTAR UMA TABUADA
+
+# 1° Passo, Entrada do número a ser somado
+numero = int(input("Digite um número para ver sua tabuada = "))
+
+# Faça as somas
+n1 = numero * 1
+n2 = numero * 2
+n3 = numero * 3
+n4 = numero * 4
+n5 = numero * 5
+n6 = numero * 6
+n7 = numero * 7
+n8 = numero * 8
+n9 = numero * 9
+n10 = numero * 10
+
+print("-" * 12)
+print("{} x  1 = {}".format(numero, n1))
+print("{} x  2 = {}".format(numero, n2))
+print("{} x  3 = {}".format(numero, n3))
+print("{} x  4 = {}".format(numero, n4))
+print("{} x  5 = {}".format(numero, n5))
+print("{} x  6 = {}".format(numero, n6))
+print("{} x  7 = {}".format(numero, n7))
+print("{} x  8 = {}".format(numero, n8))
+print("{} x  9 = {}".format(numero, n9))
+print("{} x 10 = {}".format(numero, n10))
+print("-" * 12)
