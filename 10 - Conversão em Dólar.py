60d1aa93554a2d18ec2ca97cdb70a832de4d8dc0
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 18:56:52 -0300

    Converta Reais em Dólar

---
+++ 10 - Conversão em Dólar.py
@@ -0,0 +1,15 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# CONVERTA QUALQUER VALOR EM REAIS PARA DÓLAR
+
+# 1° Passo, Entrada do valor em reais
+reais = float(input("Valor em reais = R$"))
+
+# Soma da conversão
+# Coloque sempre a cotação atual aqui e somente um exemplo
+soma = reais / 5.12
+
+print("{}R$, dara exatamente = U${:.2F}". format(reais, soma))
