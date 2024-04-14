9f5cafaa54977a4be715a4b7ae221b2f30cdb9f7
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 15:45:35 -0300

    Verificar o Tipo Primitivo

---
+++ 4 - Tipo Primitivo.py
@@ -0,0 +1,29 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# COMO INFORMAR O TIPO PRIMITIVO DE QUALQUE COISA ESCRITA NO TECLADO 
+
+# 1° Passo, Crie a Entrada do Usuario
+entrada = input("Digite Algo = ")
+
+# 2° Passo, Exibar as Informações
+print("O tipo primitivo desse valor e = ", type(entrada))
+
+# Verificar se um número
+print("E número? ", entrada.isnumeric())
+
+# Verificar se Alfanumérico
+print("E alfanumérico? ", entrada.isalnum())
+
+# Verificar se Alfabético
+print("E alfabético? ", entrada.isalpha())
+
+# E existem muitos outros tipos de verificação
+
+
+
+
+
+
