fed08ad56984271b05faf84ec604ec69bc47b98d
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-14 17:07:13 -0300

    Soma o Dobro, Triplo e a Raiz Quadrada

---
+++ 6 - Dobro, Triplo e Raiz Quadrada.py
@@ -0,0 +1,24 @@
+# 1 ()
+# **
+# * / // %
+# + -
+
+# UM PROGRAMA QUE LIA UM NÚMERO, E MOSTRE SEU DOBRO, TRIPLO E RAIZ QUADRADA
+
+# 1° Passo, Entrada do Número
+numero = int(input("Digite um Número = "))
+
+# Seu Dobro
+dobro = numero * 2
+
+# Seu Triplo
+triplo = numero * 3
+
+# Raiz Quadrada
+raiz_quadrada = numero ** (1/2)
+
+print("O dobro de {} vale = {}".format(numero, dobro))
+
+print("O triplo de {} vale = {}".format(numero, triplo))
+
+print("A raiz quadrada de {} e  = {:.2f}".format(numero,  raiz_quadrada))
