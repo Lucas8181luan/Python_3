8e19e2b43ef18c147a42b654aa5c5bad752339c1
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-04-21 12:20:44 -0300

    Número maior, menor, igual

---
+++ 038 - Número maior, menor, igual.py
@@ -0,0 +1,11 @@
+
+n1 = int(input("Pirmeiro número = "))
+
+n2 = int(input("Segundo número = "))
+
+if n1 > n2:
+    print("O PRIMEIRO número é maior")
+elif n2 > n1:
+    print("O SEGUNDO número é maior")
+else:
+    print("Os dois valores são IGUAIS")
