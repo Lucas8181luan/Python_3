f514fd85bb7c48623011fa1194f618cff244a4b5
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2024-06-06 17:50:57 -0300

    Jogo de dados em python

--- 091 - Jogo de dados em python.py
+++ 091 - Jogo de dados em python.py
@@ -3,7 +3,7 @@ import random
 dado = {}
 cont = 0
 for i in range(1, 5):
-    j_dado = random.randint(1, 7)
+    j_dado = random.randint(1, 6)
     print(f"Jogador {i} tirou {j_dado} no dado")
     dado[f"jogador {i}"] = j_dado
     time.sleep(1)
@@ -12,5 +12,5 @@ print("  == RANKING DOS JOGADORES ==")
 ordem = dict(sorted(dado.items(), key = lambda item: item[1], reverse = True))
 for i, r in ordem.items():
     cont += 1
-    print(f"  {cont}º lugar: {i} com {r}.")
+    print(f"  {cont}º lugar: {i} com = {r}.")
 print("=" * 40)
